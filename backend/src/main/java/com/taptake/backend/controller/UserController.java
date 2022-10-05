e93b3b87635c875d37b93989253e918a36b392a5
gabrielscholze-r <oliverfantasma24@gmail.com>
2022-10-05 14:13:48 -0300

    crud

--- backend/src/main/java/com/taptake/backend/controller/UserController.java
+++ backend/src/main/java/com/taptake/backend/controller/UserController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -17,9 +19,9 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<Object> savePerson(@RequestBody UserDTO userDTO){
-//        if(userService.existsByEmail(userDTO.getEmail())){
-//            return ResponseEntity.status(HttpStatus.CONFLICT).body(false);
-//        }
+        if(userService.findByEmail(userDTO.getEmail()).isPresent()){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(false);
+        }
         var user = new User();
         BeanUtils.copyProperties(userDTO, user);
         user.setPoints(0);
@@ -28,8 +30,55 @@ public class UserController {
 
     @GetMapping
     public ResponseEntity<Object> exemplo(){
-        return ResponseEntity.status(HttpStatus.OK).body("ROGERIO CENI");
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findAll());
     }
+
+    @GetMapping("/findByEmail")
+    public ResponseEntity<Object> findByEmail(@RequestParam("email") String email){
+        Optional<User> userOptional = userService.findByEmail(email);
+        if(userOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.OK).body(userOptional.get());
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(false);
+
+    }
+    @DeleteMapping("/delete")
+    public ResponseEntity<Object> deleteOne(@RequestParam("email") String email){
+        Optional<User> userOptional = userService.findByEmail(email);
+        if(userOptional.isPresent()){
+            userService.deleteOne(userOptional.get().getId());
+
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(true);
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(false);
+
+    }
+
+    @PutMapping
+    public ResponseEntity<Object> updateOne(@RequestBody UserDTO userDTO, @RequestParam("emal") String email){
+        Optional<User> userOptional = userService.findByEmail(email);
+
+        if(!userOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(false);
+        }
+        User newUser = userOptional.get();
+
+        if(userDTO.getUsername()!=newUser.getUsername()){
+            newUser.setUsername(userDTO.getUsername());
+        }
+        if(userDTO.getPhone()!=newUser.getPhone()){
+            newUser.setPhone(userDTO.getPhone());
+        }
+        var user = new User();
+        BeanUtils.copyProperties(userDTO, user);
+        user.setPoints(0);
+        return ResponseEntity.status(HttpStatus.OK).body(userService.update(user));
+    }
+
+
+
+
+
 
 
 
--- backend/src/main/java/com/taptake/backend/service/UserService.java
+++ backend/src/main/java/com/taptake/backend/service/UserService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserService {
@@ -19,11 +21,16 @@ public class UserService {
         return userRepository.save(user);
     }
 
-//    public boolean existsByEmail(String email){
-//        return userRepository.existByEmail(email);
-//    }
+    public Optional<User> findByEmail(String email){
+        return userRepository.findByEmail(email);
+    }
     public List<User> findAll(){
         return userRepository.findAll();
     }
-
+    public void deleteOne(UUID id){
+        userRepository.deleteById(id);
+    }
+    public User update(User user){
+        return userRepository.save(user);
+    }
 }
