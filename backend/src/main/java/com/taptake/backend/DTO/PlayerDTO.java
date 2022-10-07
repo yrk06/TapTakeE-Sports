6d8e9ed8949658905b6ae3c55b9ae553a27e2ff2
gabrielscholze-r <oliverfantasma24@gmail.com>
2022-10-07 17:34:49 -0300

    get all request

---
+++ backend/src/main/java/com/taptake/backend/DTO/PlayerDTO.java
@@ -0,0 +1,33 @@
+package com.taptake.backend.DTO;
+
+import java.util.UUID;
+
+public class PlayerDTO {
+    private UUID idEquipe;
+    private String nome;
+    private String cargo;
+
+    public UUID getIdEquipe() {
+        return idEquipe;
+    }
+
+    public void setIdEquipe(UUID idEquipe) {
+        this.idEquipe = idEquipe;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+}
