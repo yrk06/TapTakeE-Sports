eacfed1c4da7c4085561a2ec93bb2ccda4eb5e4b
Yerik <yerik.koslowski@gmail.com>
2022-10-09 01:27:15 -0300

    Add middleware and CORS support

---
+++ backend/src/main/java/com/taptake/backend/MvcConfig.java
@@ -0,0 +1,26 @@
+package com.taptake.backend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.taptake.backend.service.LoggerMiddleware;
+import com.taptake.backend.service.ResourceBlockedMiddleware;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("taptake.yrk06.com.br");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggerMiddleware());
+
+        // Disable this when testing in localhost
+        registry.addInterceptor(new ResourceBlockedMiddleware()).addPathPatterns("/api/**");
+    }
+}
---
+++ backend/src/main/java/com/taptake/backend/service/ResourceBlockedMiddleware.java
@@ -0,0 +1,17 @@
+package com.taptake.backend.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class ResourceBlockedMiddleware implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object handler) throws Exception {
+        response.setStatus(401);
+        return false;
+    }
+}
