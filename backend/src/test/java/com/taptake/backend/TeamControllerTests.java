97342caed8a2fda460acc9c983c17deff48304f4
Gabriel Scholze Rosa <62766793+gabrielscholze-r@users.noreply.github.com>
2022-10-17 18:47:18 -0300

    Update backend/src/test/java/com/taptake/backend/TeamControllerTests.java
    Co-authored-by: Yerik <Yerik.koslowski@gmail.com>

--- backend/src/test/java/com/taptake/backend/TeamControllerTests.java
+++ backend/src/test/java/com/taptake/backend/TeamControllerTests.java
@@ -242,7 +242,7 @@ class TeamControllerTests {
         teamDTO.setIdOrg(idOrg.toString());
         teamDTO.setIdJogo(idJogo.toString());
         ResponseEntity<?> re = teamController.update(teamDTO, UUID.randomUUID().toString());
-        assertEquals(HttpStatus.CONFLICT, re.getStatusCode());
+        assertEquals(HttpStatus.BAD_REQUEST, re.getStatusCode());
     }
     @Test
     void updateTeamInvalidTeam(){
