5566b387c2d0de3611479fe347fc86d33627574b
gabrielscholze-r <oliverfantasma24@gmail.com>
2022-10-17 21:16:29 -0300

    match controller

---
+++ backend/src/main/java/com/taptake/backend/DTO/MatchDTO.java
@@ -0,0 +1,14 @@
+package com.taptake.backend.DTO;
+
+public class MatchDTO {
+
+    private String idCampeonato;
+
+    public String getIdCampeonato() {
+        return idCampeonato;
+    }
+
+    public void setIdCampeonato(String idCampeonato) {
+        this.idCampeonato = idCampeonato;
+    }
+}
---
+++ backend/src/main/java/com/taptake/backend/controller/MatchController.java
@@ -0,0 +1,78 @@
+package com.taptake.backend.controller;
+
+import com.taptake.backend.DTO.MatchDTO;
+import com.taptake.backend.model.Championship;
+import com.taptake.backend.model.Match;
+import com.taptake.backend.service.ChampionshipService;
+import com.taptake.backend.service.MatchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/match")
+public class MatchController {
+
+    @Autowired
+    private MatchService matchService;
+
+    @Autowired
+    private ChampionshipService cs;
+
+    @PostMapping
+    public ResponseEntity<Object> save(@RequestBody MatchDTO matchDTO){
+        Optional<Championship> optionalChampionship = cs.findById(UUID.fromString(matchDTO.getIdCampeonato()));
+        if(!optionalChampionship.isPresent()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        var match = new Match();
+        match.setChampionship(optionalChampionship.get());
+        return ResponseEntity.status(HttpStatus.CREATED).body(matchService.save(match));
+    }
+
+    @GetMapping("/id")
+    public ResponseEntity<Object> findById(@RequestParam String id){
+        Optional<Match> optionalMatch = matchService.findById(UUID.fromString(id));
+        if(!optionalMatch.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(optionalMatch.get());
+    }
+    @GetMapping("/champ")
+    public ResponseEntity<Object> findByCampeonato(@RequestParam String idCampeonato){
+        Optional<Championship> optionalC = cs.findById(UUID.fromString(idCampeonato));
+        if(!optionalC.isPresent()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(matchService.findByCampeonado(optionalC.get()));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Object> deleteOne(@RequestParam String id){
+        matchService.delete(UUID.fromString(id));
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Object> update(@RequestBody MatchDTO matchDTO, @RequestParam String id){
+        Optional<Match> optionalMatch = matchService.findById(UUID.fromString(id));
+        if(!optionalMatch.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        Optional<Championship> optionalC = cs.findById(UUID.fromString(matchDTO.getIdCampeonato()));
+        if(!optionalC.isPresent()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        Match m = optionalMatch.get();
+        if(!optionalMatch.get().getChampionship().getIdCampeonato().toString().equals(matchDTO.getIdCampeonato())){
+            m.setChampionship(optionalC.get());
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(matchService.update(m));
+    }
+
+
+}
