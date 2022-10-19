d219a28579d7047d426373499fb9fadfb4610bb7
alex- <alex-dambros@hotmail.com>
2022-10-19 19:58:52 -0300

    refactor database

--- backend/sql/createDatabase.sql
+++ backend/sql/createDatabase.sql
@@ -14,11 +14,11 @@ INSERT INTO Cargo (nome) VALUES ("Admin");
 
 CREATE TABLE Usuario (
     idUsuario CHAR(36) NOT NULL PRIMARY KEY,
-    idCargo INT NOT NULL,
-    FOREIGN KEY (idCargo) REFERENCES Cargo (idCargo), 
+    idCargo INT NOT NULL DEFAULT 0,
+    FOREIGN KEY (idCargo) REFERENCES Cargo (idCargo),
     nome VARCHAR(255) NOT NULL,
     email VARCHAR(512) NOT NULL UNIQUE,
-    senha VARCHAR(60) NOT NULL,	
+    senha VARCHAR(60) NOT NULL,
     telefone VARCHAR(32) NOT NULL
 );
 
@@ -76,8 +76,8 @@ CREATE TABLE ParticipacaoCampeonato (
     idCampeonato CHAR(36) NOT NULL,
     FOREIGN KEY(idCampeonato) REFERENCES Campeonato (idCampeonato),
 	idEquipe CHAR(36) NOT NULL,
-    FOREIGN KEY(idEquipe) REFERENCES Equipe (idEquipe),
-    pontuacao INT NOT NULL
+    FOREIGN KEY(idEquipe) REFERENCES Equipe (idEquipe)
+
 );
 
 CREATE TABLE Partida (
@@ -86,9 +86,9 @@ CREATE TABLE Partida (
 	FOREIGN KEY(idCampeonato) REFERENCES Campeonato (idCampeonato)
 );
 
-
 CREATE TABLE ParticipacaoPartida (
-    idParticipacaoPartida CHAR(36) NOT NULL PRIMARY KEY,
+    idParticipacaoPartida INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (idParticipacaoPartida),
 	idPartida CHAR(36) NOT NULL,
 	FOREIGN KEY(idPartida) REFERENCES Partida (idPartida),
     idEquipe CHAR(36) NOT NULL,
@@ -110,4 +110,14 @@ CREATE TABLE JogadorTimeUsuario (
 	FOREIGN KEY(idEquipeUsuario) REFERENCES TimeUsuario (idEquipeUsuario),
     idJogador CHAR(36) NOT NULL,
 	FOREIGN KEY(idJogador) REFERENCES Jogador (idJogador)
+);
+
+CREATE TABLE PerformancePartida(
+    idPerformancePartida INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (idPerformancePartida),
+    idPartida CHAR(36) NOT NULL,
+    FOREIGN KEY (idPartida) REFERENCES Partida(idPartida),
+    idJogador CHAR(36) NOT NULL,
+    FOREIGN KEY (idJogador) REFERENCES Jogador(idJogador),
+    pontuacao INT NOT NULL
 );
