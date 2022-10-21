02d7f8019573adb2a9bbb137ca622c594d2004f2
oMullerr <leindorfmuller@gmail.com>
2022-10-20 23:33:51 -0300

    add internal server error

--- backend/src/main/webapp/src/components/ErrorPage.js
+++ backend/src/main/webapp/src/components/ErrorPage.js
@@ -2,19 +2,22 @@ const { useSearchParams } = require("react-router-dom");
 const { default: Header } = require("./Header");
 const { default: NotFoundContent } = require("./NotFoundContent");
 const { default: UnauthorizedContent } = require("./UnauthorizedContent");
-
+const { default: InternalServerError } = require("./InternalServerError");
 
 const ErrorPage = () => {
-    const error = useSearchParams()[0].get("error")
-    return (
-        <div>
-            <Header />
-            {{
-                '401': <UnauthorizedContent />,
-                '404': <NotFoundContent />,
-            }[error]}
-        </div>
-    );
-}
+  const error = useSearchParams()[0].get("error");
+  return (
+    <div>
+      <Header />
+      {
+        {
+          401: <UnauthorizedContent />,
+          404: <NotFoundContent />,
+          500: <InternalServerError />,
+        }[error]
+      }
+    </div>
+  );
+};
 
 export default ErrorPage;
--- backend/src/main/webapp/src/setupProxy.js
+++ backend/src/main/webapp/src/setupProxy.js
@@ -1,28 +1,26 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-    app.use(
-        '/api',
-        createProxyMiddleware({
-            target: 'http://localhost:8090',
-            changeOrigin: true,
-        })
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: "http://localhost:8090",
+      changeOrigin: true,
+    })
+  );
+  // app.use(
+  //     '/login',
+  //     createProxyMiddleware({
+  //         target: 'http://localhost:8090',
+  //         changeOrigin: true,
+  //     })
 
-    );
-    app.use(
-        '/login',
-        createProxyMiddleware({
-            target: 'http://localhost:8090',
-            changeOrigin: true,
-        })
-
-    );
-    app.use(
-        '/logout',
-        createProxyMiddleware({
-            target: 'http://localhost:8090',
-            changeOrigin: true,
-        })
-
-    );
+  // );
+  app.use(
+    "/logout",
+    createProxyMiddleware({
+      target: "http://localhost:8090",
+      changeOrigin: true,
+    })
+  );
 };
