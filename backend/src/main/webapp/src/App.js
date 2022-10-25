117bf0aa2ff6e0a61cad727d01e169730041f8bf
Yerik K <yerik.koslowski@gmail.com>
2022-10-25 18:18:02 -0300

    Fixed tests not passing

--- backend/src/main/webapp/src/App.js
+++ backend/src/main/webapp/src/App.js
@@ -15,13 +15,6 @@ import Cast from "./components/Cast";
 import Lineup from "./components/Lineup";
 import UpdateForm from "./components/UpdateForm";
 import CreateForm from "./components/CreateForm";
-
-import Header from './components/Header'
-import HomePageContent from './components/HomePageContent'
-import ErrorPage from './components/ErrorPage'
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import LoginForm from './components/LoginForm';
 import SignupForm from './components/SignupForm';
 
 function App() {
@@ -60,77 +53,77 @@ function App() {
             element: <SignupForm />
           },
           {
-          path: "/UpdateForm",
-          element: (
-            <div>
-              <Header signed={signed} />
-              <UpdateForm />
-            </div>
-          ),
-        },
-        {
-          path: "/CreateForm",
-          element: (
-            <div>
-              <Header signed={signed} />
-              <CreateForm />
-            </div>
-          ),
-        },
-        {
-          path: "/Cast",
-          element: (
-            <div>
-              <Header signed={signed} />
-              <Cast />
-            </div>
-          ),
-        },
-        {
-          path: "/Lineup",
-          element: (
-            <div>
-              <Header signed={signed} />
-              <Lineup />
-            </div>
-          ),
-        },
-        {
-          path: "/about",
-          element: (
-            <div>
-              <Header signed={signed} />
-              <About />
-            </div>
-          ),
-        },
-        {
-          path: "/howItWorks",
-          element: (
-            <div>
-              <Header signed={signed} />
-              <HowItWorks />
-            </div>
-          ),
-        },
-        {
-          path: "/ViewRecoverySearchUser",
-          element: (
-            <div>
-              <Header signed={signed} />
-              <ViewRecoverySearchUser />
-            </div>
-          ),
-        },
-        {
-          path: "/ViewRecoverySearchAdmin",
-          element: (
-            <div>
-              <Header signed={signed} />
-              <ViewRecoverySearchAdmin />
-            </div>
-          ),
-        },
+            path: "/UpdateForm",
+            element: (
+              <div>
+                <Header signed={signed} />
+                <UpdateForm />
+              </div>
+            ),
+          },
+          {
+            path: "/CreateForm",
+            element: (
+              <div>
+                <Header signed={signed} />
+                <CreateForm />
+              </div>
+            ),
+          },
+          {
+            path: "/Cast",
+            element: (
+              <div>
+                <Header signed={signed} />
+                <Cast />
+              </div>
+            ),
+          },
+          {
+            path: "/Lineup",
+            element: (
+              <div>
+                <Header signed={signed} />
+                <Lineup />
+              </div>
+            ),
+          },
+          {
+            path: "/about",
+            element: (
+              <div>
+                <Header signed={signed} />
+                <About />
+              </div>
+            ),
+          },
+          {
+            path: "/howItWorks",
+            element: (
+              <div>
+                <Header signed={signed} />
+                <HowItWorks />
+              </div>
+            ),
+          },
+          {
+            path: "/ViewRecoverySearchUser",
+            element: (
+              <div>
+                <Header signed={signed} />
+                <ViewRecoverySearchUser />
+              </div>
+            ),
+          },
+          {
+            path: "/ViewRecoverySearchAdmin",
+            element: (
+              <div>
+                <Header signed={signed} />
+                <ViewRecoverySearchAdmin />
+              </div>
+            ),
+          },
         ])
       } />
     </div>
