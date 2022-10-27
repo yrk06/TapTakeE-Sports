3b274fd4acd49a2d7a774312648b96ded92e2a56
gabrielscholze-r <oliverfantasma24@gmail.com>
2022-10-27 20:07:24 -0300

    model, service and repository

---
+++ backend/src/main/java/com/taptake/backend/model/ChampionshipParticipation.java
@@ -0,0 +1,48 @@
+package com.taptake.backend.model;
+
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "ParticipacaoCampeonato")
+public class ChampionshipParticipation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Type(type = "uuid-char")
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "idCampeonato")
+    private Championship championship;
+
+    @OneToOne
+    @JoinColumn(name = "idEquipe")
+    private Team team;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Championship getChampionship() {
+        return championship;
+    }
+
+    public void setChampionship(Championship championship) {
+        this.championship = championship;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+}
