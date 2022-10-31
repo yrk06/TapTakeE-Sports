931ef79e92bc11f0e7446593167a9f5fd1413032
oMullerr <leindorfmuller@gmail.com>
2022-10-30 21:57:47 -0300

    add GamesForm and TeamsForm

---
+++ backend/src/main/webapp/src/components/GamesForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class GameForm extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>CRIAR UM NOVO JOGO NESSA PAGINA</p>
+            </div>
+        );
+    }
+}
+
+export default GameForm;
